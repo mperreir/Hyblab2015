35a8ef7090adabaf62cc7294fa5e1fb6b0fe08ee
Claire REMY <anha.flint@gmail.com>
2015-01-28 15:34:19 +0000

    update : swig template integration

--- le_journal_des_entreprises/server-api.js
+++ le_journal_des_entreprises/server-api.js
@@ -2,10 +2,15 @@
 var express = require('express');
 var path = require('path');
 var request = require('request');
+var swig = require('swig');
 var app = express();
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/html');
+
 // serve static content from the html directory
-app.use(express.static(path.join(__dirname, 'html')));
+//app.use(express.static(path.join(__dirname, 'html')));
 // also add the path of the libs that are stored in our node_modules directory 
 app.use('/angular',express.static(path.join(__dirname, 'node_modules/angular')));
 app.use('/bootstrap',express.static(path.join(__dirname, 'node_modules/bootstrap')));
@@ -13,13 +18,14 @@ app.use('/jquery',express.static(path.join(__dirname, 'node_modules/jquery')));
 app.use('/d3',express.static(path.join(__dirname, 'node_modules/d3')));
 
 // if the server is asked for some data, request it from data.nantes.fr and send it back to the browser
-app.get('/data', function(req,res){
+app.get('/', function(req,res){
   // request object come from the 'request' module for nodejs. It simplifies requests programming.  See its documentation for more details
-  request('http://data.nantes.fr/api/getDisponibiliteParkingsPublics/1.0/39W9VSNCSASEOGV/?output=json', function (error, response, body) {
+/*  request('http://data.nantes.fr/api/getDisponibiliteParkingsPublics/1.0/39W9VSNCSASEOGV/?output=json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(body);
     }
-  })
+  })*/
+    res.render('index', {test: 'coucou'});
 })
 
 // launch the server
