814a9d6454d845fc9b999b68682e46349c33455a
hyblab_tweens <hyblab.tweens@gmail.com>
2015-01-30 00:08:04 +0000

    modif fichier test/test.js

--- test/test.js
+++ test/test.js
@@ -3,8 +3,20 @@ var http = require("http");
 var server = http.createServer(
     function(req, res)
     {
-        res.writeHead(200);
-        res.end("Salut tout le monde");
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(
+            '<!DOCTYPE html>'+
+            '<html lang="fr">'+
+            '<head>'+
+            '<meta charset="utf-8" />'+
+            '<title>Test</title>'+
+            '</head>'+
+            '<body>'+
+            '<p>ceci est un texte encrit en <strong>html</strong>.</p>'+
+            '</body>'+
+            '</html>');
+        
+        res.end();
     }
 );
 
