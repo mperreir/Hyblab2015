f1ca039226a1c95fa759560b1c2ca1f903fd8573
hyblab_tweens <hyblab.tweens@gmail.com>
2015-01-30 09:23:20 +0000

    modif test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 var http = require("http");
+var url = require("url");
+var querystring = require('querystring');
 
 var server = http.createServer(
     function(req, res)
     {
         res.writeHead(200, {"Content-Type": "text/html"});
+        var page = url.parse(req.url).pathname;
+        var params = querystring.parse(url.parse(req.url).query);
+        console.log(page);
         res.write(
             '<!DOCTYPE html>'+
             '<html lang="fr">'+
@@ -12,7 +17,17 @@ var server = http.createServer(
             '<title>Test</title>'+
             '</head>'+
             '<body>'+
-            '<p>ceci est un texte encrit en <strong>html</strong>.</p>'+
+            '<p>ceci est un texte encrit en <strong>html</strong>.</p>');
+        if('nom' in params && 'prenom' in params)
+        {
+            res.write('<p>vous vous appelez: ' + params['nom'] + ' ' + params['prenom'] + '.</p>');
+        }
+        else
+        {
+            res.write('<p>vous devez avoir un nom et un prénom, non?!</p>');
+        }
+            
+        res.write(
             '</body>'+
             '</html>');
         
