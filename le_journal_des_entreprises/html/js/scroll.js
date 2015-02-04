aed2067e37b73a7eb8d22df66891e323f240b271
ayssti <benjamin.mouden@gmail.com>
2015-02-04 18:57:23 +0000

    cette fois ça marche vraimment, slide 1 et 2 faites (manque adaptation grand et petits écrans)

---
+++ le_journal_des_entreprises/html/js/scroll.js
@@ -0,0 +1,32 @@
+function parallax() {
+	    setpos("#inside2");
+    setpos("#inside1");
+
+}
+
+
+
+function setpos(element, factor) {
+    factor = factor || 2;
+    
+    var offset = $(element).offset();
+    var w = $(window);
+    
+    var posy = (offset.top - w.scrollTop()) / factor;
+    
+    $(element).css('background-position', '50% '+posy+'px');
+
+
+}
+
+$(document).ready(function () {
+    	$('a[rel="relativeanchor"]').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 1000);
+	    return false;
+	}); 
+    parallax();
+}).scroll(function () {
+    parallax();
+});
