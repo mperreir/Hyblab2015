8cfc6ad26e669ee7edac256725bb278e9542a423
xalor <quentin.-@hotmail.fr>
2015-02-21 11:31:05 +0000

    animation_outro_pour_pull

---
+++ le_journal_des_entreprises/html/js/outro.js
@@ -0,0 +1,6 @@
+$('#merci').mouseover(function() {
+   //$(this).toggleClass('active');
+    $(this).css("transform","rotate(45deg)");
+    $(this).css("transform","rotate(-90deg)");
+    $(this).css("transform","rotate(45deg)");
+});
