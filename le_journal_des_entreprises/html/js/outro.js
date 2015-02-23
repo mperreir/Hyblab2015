953e96d233198567da2334226307d55e1e086023
ayssti <benjamin.mouden@gmail.com>
2015-02-23 20:28:14 +0000

    merge outro correct

--- le_journal_des_entreprises/html/js/outro.js
+++ le_journal_des_entreprises/html/js/outro.js
@@ -7,9 +7,3 @@ $('#casierD').on('click', function() {
 $('#coatRack').on('click', function() {
     $('#modalEquipe')  .modal();
 })
-
-<<<<<<< HEAD
-$('.modal-dialog').css('width',this.find('img').width());
-=======
-$('.modal-dialog').css('width'
->>>>>>> 90b08902b22b60a1cdc5508ba25c25321e1509c3
